b4f3a3c4e1a8c42673f8f660f7b93812f760519d
TheseCookiez <93494201+TheseCookiez@users.noreply.github.com>
2022-11-19 16:02:15 +0000

    Update with arguments

--- ip_scanner.py
+++ ip_scanner.py
@@ -1,12 +1,29 @@
 import concurrent.futures
 import os
+import sys
+import socket
 
+argv = sys.argv
 ip_addresses = []
-usr_input = str(input("[+] Enter the IP range you want to scan (i.e 192.168.1.0)\n"))
-subnet = usr_input.rsplit('.',1)[0]
-print(f"Scanning all IPs in subnet {subnet}.0-255")
-for i in range(0,255):
-	ip_addresses.append(f"{subnet}.{i}")
+
+
+def scan_current_subnet():
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.connect(("8.8.8.8", 80))
+	ip = s.getsockname()[0]
+	subnet = ip.rsplit('.',1)[0]
+	print(f"[+] Created IP list of {subnet}.0-255")
+	for i in range(0,255):
+		ip_addresses.append(f"{subnet}.{i}")
+	main()
+
+def user_defiend_subnet():
+	usr_input = str(input("[+] Enter the IP range you want to scan (i.e 192.168.1.0)\n"))
+	subnet = usr_input.rsplit('.',1)[0]
+	print(f"[+] Created IP list of {subnet}.0-255")
+	for i in range(0,255):
+		ip_addresses.append(f"{subnet}.{i}")
+	main()
 
 def ping(ip):
 	response = os.system(f"ping -t2 -c1 {ip} >/dev/null")
@@ -19,5 +36,29 @@ def main():
 	with concurrent.futures.ThreadPoolExecutor() as executor:
 		executor.map(ping, ip_addresses)
 
-if __name__ == '__main__':
-	main()
+
+if len(argv) < 2:
+	print("""
+		No argument was given, printing help (-h)
+
+		-h		Display this help message
+		-a		Scan all IPs in current subnet
+		-s		Scan spcific subnet (i.e 192.168.1.0)
+
+		""")
+	exit()
+elif argv[1] == "-h":
+	print("""
+		Printing help (-h)
+
+		-h		Display this help message
+		-a		Scan all IPs in current subnet
+		-s		Scan spcific subnet (i.e 192.168.1.0)
+
+		""")
+	exit()
+elif argv[1] == "-a":
+	print("[+] Getting current subnet...")
+	scan_current_subnet()
+elif argv[1] == "-s":
+	user_defiend_subnet()
